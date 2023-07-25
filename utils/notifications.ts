2b8bc75448ce039f35d140482c6d13f15e1f3dc6
Maksymilian Furtak <maksymilian.furtak@embiq.com>
2023-07-25 15:30:39 +0200

    Catch errors of notifications handlers

--- utils/notifications.ts
+++ utils/notifications.ts
@@ -7,54 +7,69 @@ import { useEffect, useState } from "react";
  * @link https://rnfirebase.io/messaging/usage#ios---requesting-permissions
  */
 export const requestMessagingPermission = async () => {
-  const authStatus = await messaging().requestPermission();
-  const enabled =
-    authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
-    authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+  try {
+    const authStatus = await messaging().requestPermission();
+    const enabled =
+      authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+      authStatus === messaging.AuthorizationStatus.PROVISIONAL;
 
-  if (enabled) {
-    console.log("Authorization status:", authStatus);
+    if (enabled) {
+      console.log("Authorization status:", authStatus);
+    }
+  } catch (error) {
+    console.error(error);
   }
 };
 
 /** Register handler for Background & Quit state messages
  * @link https://rnfirebase.io/messaging/usage#background--quit-state-messages
  * */
-export const registerBackgroundMessageHandler = () =>
-  messaging().setBackgroundMessageHandler(async (remoteMessage) => {
-    console.log("Message handled in the background!", remoteMessage);
-  });
+export const registerBackgroundMessageHandler = () => {
+  try {
+    return messaging().setBackgroundMessageHandler(async (remoteMessage) => {
+      console.log("Message handled in the background!", remoteMessage);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 export const registerMessageHandler = () => {
-  if (requestMessagingPermission()) {
-    messaging()
-      .getToken()
-      .then(async (token) => {
-        await storeData("fcmToken", token);
+  try {
+    requestMessagingPermission()
+      .then(() => {
+        messaging()
+          .getToken()
+          .then(async (token) => {
+            await storeData("fcmToken", token);
+          });
+      })
+      .catch(() => {
+        console.log("Request messaging permission error");
       });
-  } else {
-    console.log("Request messaging permission error");
-  }
 
-  // Check whether an initial notification is available
-  messaging()
-    .getInitialNotification()
-    .then(async (remoteMessage) => {
-      if (remoteMessage) {
-        console.log(
-          "Notification caused app to open from quit state:",
-          remoteMessage.notification
-        );
-      }
+    // Check whether an initial notification is available
+    messaging()
+      .getInitialNotification()
+      .then(async (remoteMessage) => {
+        if (remoteMessage) {
+          console.log(
+            "Notification caused app to open from quit state:",
+            remoteMessage.notification
+          );
+        }
+      });
+
+    // Assume a message-notification contains a "type" property in the data payload of the screen to open
+    messaging().onNotificationOpenedApp(async (remoteMessage) => {
+      console.log(
+        "Notification caused app to open from background state:",
+        remoteMessage.notification
+      );
     });
-
-  // Assume a message-notification contains a "type" property in the data payload of the screen to open
-  messaging().onNotificationOpenedApp(async (remoteMessage) => {
-    console.log(
-      "Notification caused app to open from background state:",
-      remoteMessage.notification
-    );
-  });
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 export const unsubscribeMessageHandler = messaging().onMessage(
