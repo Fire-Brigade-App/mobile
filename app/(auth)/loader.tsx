4cf0977ccbcd1728a09fdf166320f605c0fb1a67
Maksymilian Furtak <maksymilian.furtak@embiq.com>
2023-07-25 15:28:28 +0200

    Add Loader as initial page

---
+++ app/(auth)/loader.tsx
@@ -0,0 +1,8 @@
+import React, { FC } from "react";
+import { ScreenLoader } from "../../components/loader/ScreenLoader";
+
+const Loader: FC = () => {
+  return <ScreenLoader />;
+};
+
+export default Loader;
