13138b349476a784d9a7604ef9b02d2aa259d977
Maksymilian Furtak <maksymilian.furtak@embiq.com>
2023-08-01 14:57:03 +0200

    Pre-release 0.1.3

--- app.config.js
+++ app.config.js
@@ -4,7 +4,7 @@ export default {
   expo: {
     name: "Fire Brigade",
     slug: "fire-brigade",
-    version: "0.1.2",
+    version: "0.1.3",
     scheme: "fire-brigade",
     // web: {
     //   bundler: "metro",
